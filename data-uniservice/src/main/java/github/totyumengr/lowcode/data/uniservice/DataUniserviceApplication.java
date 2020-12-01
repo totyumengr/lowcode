04116f3f9fd3734390468327fd9700a594149237
rami <totyumengr@gmail.com>
2020-12-01 16:47:40 +0800

    add data-uniservice project

---
+++ data-uniservice/src/main/java/github/totyumengr/lowcode/data/uniservice/DataUniserviceApplication.java
@@ -0,0 +1,13 @@
+package github.totyumengr.lowcode.data.uniservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataUniserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataUniserviceApplication.class, args);
+	}
+
+}
---
+++ data-uniservice/src/test/java/github/totyumengr/lowcode/data/uniservice/DataUniserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package github.totyumengr.lowcode.data.uniservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataUniserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
